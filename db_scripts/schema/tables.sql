a0994c92f231f7813e9b219faad397e27b001b0a
Mimi97-aqua <ebenyemiriam17@gmail.com>
2025-07-26 17:32:15 +0100

    fix(db): add deleted_at field to members table

--- db_scripts/schema/tables.sql
+++ db_scripts/schema/tables.sql
@@ -46,7 +46,8 @@ create table members (
     invited_by_id uuid references members(id),
     modified_by_id uuid references members(id),
     created_at timestamp with time zone not null default current_timestamp,
-    updated_at timestamp with time zone
+    updated_at timestamp with time zone,
+    deleted_at timestamp with time zone
 );
 
 -- teams table
